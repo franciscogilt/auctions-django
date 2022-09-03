9750ba13d53c2f8a6f2f38e3b940b47eb6e36a0f
Francisco Gil <franciscogilt@gmail.com>
2022-09-03 12:26:44 -0400

    add base html templates and views

--- auctions/views.py
+++ auctions/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "auctions/index.html", {})
+
+
+def login_view(request):
+    return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    return render(request, "auctions/register.html")
