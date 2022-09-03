2d5b83a39defd150043fc6161f8c4ff4c1e9fe97
Francisco Gil <franciscogilt@gmail.com>
2022-09-03 12:29:28 -0400

    implement register

--- auctions/views.py
+++ auctions/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-
+from .models import User
 # Create your views here.
 
 
@@ -21,4 +22,27 @@ def logout_view(request):
 
 
 def register(request):
-    return render(request, "auctions/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
