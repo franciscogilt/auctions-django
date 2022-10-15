7cefc10b5a65150cb352dc8e23ac82d49bc270d7
Francisco Gil <franciscogilt@gmail.com>
2022-10-15 09:50:48 -0400

    prepare for deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django.contrib.sites",
-    'sslserver',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
